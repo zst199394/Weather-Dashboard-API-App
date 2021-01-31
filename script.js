83502d915b93ecd2d7e487202f5a072e21a4a427
shuting <zst9394@outlook.com>
2021-01-31 13:27:58 -0500

    add todayweather

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ $("#btn1").on("click",function(event){
     $(".page2").removeAttr("style");
     console.log("this function work !!!");
 });
-$("#searchBtn").on("click",function(event){
-     event.preventDefault();
-     var userInput =$("#input").val().trim();
-     var cities = JSON.parse(localStorage.getItem("city")) || [];
-     console.log("This function work too !")
-     localStorage.setItem("city",JSON.stringify(userInput));
-     for(var i=0;i< cities.length; i++){
-     var a=$("<li>");
-     a.text(cities[i]);
-     $("#recentSearch"),append(a);
-     }
+
+// var cities = JSON.parse(localStorage.getItem("city"));// || [];
+// function showHistory () {
+//   console.log("showHistory function works!", cities)
+//   for (var i=0; i< cities.length; i++){
+//       var a = $("<li>");
+//       a.text(cities[i]);
+//       $("#recentSearch").append(a);
+//   }
+// }
+// showHistory()
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var userInput = $('#input').val().trim();
+
+    // cities.push(userInput)
+    // localStorage.setItem("city", JSON.stringify(cities));
+    // showHistory()
+    // console.log(cities)
 
 
-console.log(userInput);
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +userInput+ "&appid=adb5289082d713294f40248da64a0cf5";
 $.ajax({
     url: queryURL,
@@ -25,19 +33,28 @@ $.ajax({
 }).then(function(response){
   console.log("currentday:")
   console.log(response)
-//   console.log(responsed.coord.lon)
-  
-});
-// var lat=
-// var lon=
-// var queryURL ="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&appid=adb5289082d713294f40248da64a0cf5";
+  var cityLon =response.coord.lon
+  var cityLat =response.coord.lat
+  var queryURL ="https://api.openweathermap.org/data/2.5/onecall?lat="+cityLat+"&lon="+cityLon+"&exclude=minutely,hourly,alerts&appid=adb5289082d713294f40248da64a0cf5"
+
 $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly,alerts&appid=adb5289082d713294f40248da64a0cf5",
-    //https://api.openweathermap.org/data/2.5/onecall?q=orlando&exclude=daily&appid=adb5289082d713294f40248da64a0cf5
-    //api.openweathermap.org/data/2.5/forecast?q=orlando&appid=adb5289082d713294f40248da64a0cf5
+    url: queryURL,
     method: "GET"
 }).then(function(response){
     console.log("5daysresponse:")
     console.log(response)
+    var b =$("<div>")
+    b.text("Today Weather("+userInput+")")
+    var c=$("<div>")
+    c.text("Temp:"+response.current.temp+"°F")
+    var d=$("<div>")
+    d.text("Humidity:"+response.current.humidity+"%")
+    var e=$("<div>")
+    e.text("Wind Speed:"+response.current.weather.wind_speed+"MPH")
+    var f=$("<div>")
+    f.text("UV Index:"+response.current.uvi)
+    $("#currentWeather").append(b,c,d,e,f);
+    
+});
 });
 });
